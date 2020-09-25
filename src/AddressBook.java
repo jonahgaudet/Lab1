65a837e1238ee65c59dbc12191d4755166765d4e
Jonah <jonahgaudet@rogers.com>
2020-09-25 09:35:45 -0400

    git commit

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,8 +4,11 @@ public class AddressBook {
 
     public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("Tom", 20, "123 Main Street", "6130123456");
+        BuddyInfo buddy2 = new BuddyInfo("Jack", 56, "5 North road", "6136543210");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
+        addressBook.addBuddy(buddy2);
+        addressBook.removeBuddy(buddy2);
         addressBook.removeBuddy(buddy);
     }
 
